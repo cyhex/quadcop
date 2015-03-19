4a7be795f9fe887e7ae3b341ee3a2952945ddc60
Timor A <timor@cyhex.com>
2015-03-19 13:49:26 +0100

    fixed pid

--- Arduino/flighPlatform/lib/libs/Wire/Wire.cpp.d
+++
@@ -1,9 +0,0 @@
-lib/libs/Wire/Wire.cpp.o: \
- /home/gx/bin/arduino-1.0.6/libraries/Wire/Wire.cpp \
- /home/gx/bin/arduino-1.0.6/libraries/Wire/utility/twi.h \
- /home/gx/bin/arduino-1.0.6/libraries/Wire/Wire.h \
- /home/gx/bin/arduino-1.0.6/hardware/arduino/cores/arduino/Stream.h \
- /home/gx/bin/arduino-1.0.6/hardware/arduino/cores/arduino/Print.h \
- /home/gx/bin/arduino-1.0.6/hardware/arduino/cores/arduino/WString.h \
- /home/gx/bin/arduino-1.0.6/hardware/arduino/cores/arduino/Printable.h \
- /home/gx/bin/arduino-1.0.6/hardware/arduino/cores/arduino/new.h
